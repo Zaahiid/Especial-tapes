67f51dd4217bce79f86cde039e5bd8f473a8acb8
Zaahiid <zahidrsheikh01@gmail.com>
2024-05-27 00:27:53 +0530

    Initial commit - Basic Setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='w-full h-[100vh] bg-gradient-to-r from-[#faf9f9] to-[#f6fff8] flex flex-col justify-center items-center'>
+        <h1 className='text-5xl font-bold'>Especial Tapes</h1>
+        <h3 className='my-5 text-2xl animate-pulse text-[#a4133c]'>Coming Soon... 📌</h3>
+    </div>
+  )
+}
+
+export default Home
